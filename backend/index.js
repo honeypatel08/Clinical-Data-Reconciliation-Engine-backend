c040cbc82986209a1d5b4198807acd0818d944cf
Honey P. <honeynaileshpatel@gmail.com>
2026-03-18 22:46:46 -0400

    cors policy updated

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
